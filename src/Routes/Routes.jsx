52ea2ea5df2c036d295ce74b13994d46d0ed8ed1
Mahir-H07 <ttxmahir@gmail.com>
2025-10-29 20:26:26 +0600

    react-router-setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+
+
+
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+
+
+ export  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h3>Hello World dghghdf</h3>
+  }
+]);
