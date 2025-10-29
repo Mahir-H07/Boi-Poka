537ffffebb841e37ce32b3d3ef1a08aa0191413d
Mahir-H07 <ttxmahir@gmail.com>
2025-10-29 21:08:35 +0600

    navbar-added

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>page not found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
